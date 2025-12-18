e452c2bf3838527ca2408fa69ee9726aa4bbb175
adrian-jackson <67124983+adrian-jackson@users.noreply.github.com>
2025-12-18 10:49:14 -0500

    remove key

--- inference_live.py
+++ inference_live.py
@@ -16,12 +16,12 @@ cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
 pose_model = get_model(
     model_id="pose_calculator/14",
-    api_key="YB4rtgGaV7id93lETrRc"
+    api_key="API_KEY"
 )
 
 fill_model = get_model(
     model_id="fill_calculator/3",
-    api_key="YB4rtgGaV7id93lETrRc"
+    api_key="API_KEY"
 )
 
 box_annotator = sv.BoxAnnotator()
